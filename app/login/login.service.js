6540e755f01bb60c9a68ae9156bf679f7c243cb1
OlaBKarlsson <ola.karlsson@nackademin.se>
2017-03-16 15:48:14 +0100

    corrected bug

--- app/login/login.service.js
+++ app/login/login.service.js
@@ -1,13 +1,15 @@
 angular.module("login")
     .factory("loginService", ["$http", function ($http) {
         var isLoggedIn=false;
-        var 
+        var loginId;
+        var userRole;
         return {
             login: function(obj){
                 return $http.post("http://nackademiska-api.azurewebsites.net/api/account/login", obj);         
             },
              setUserData:function(userid, role){
                 loginId=userid;
+                userRole=role;
                 isLoggedIn=true;
             },
             isLoggedIn: function(){
