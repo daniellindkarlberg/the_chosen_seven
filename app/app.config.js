c78d015aca657e49537a3e5e4048a74f401931e7
daniellindkarlberg <danielskua@hotmail.com>
2017-03-16 11:40:17 +0100

    added stuff

---
+++ app/app.config.js
@@ -0,0 +1,25 @@
+angular.module("app")
+.config(["$routeProvider", "$locationProvider", 
+function($routeProvider, $locationProvider) {
+    $routeProvider
+    .when("/", { 
+        templateUrl: "app/auction/auction-list.template.html",
+        controller: "appController"
+    })
+    .when("/login", {
+        templateUrl: "app/login/login.template.html",
+        controller: "loginController"
+    })
+    
+    .when("/admin", {
+        templateUrl: "app/admin/admin.template.html",
+        controller: "adminController"
+    })
+    .when("/customer", {
+        templateUrl: "app/customer/customer.template.html",
+        controller: "customerController"
+    })
+ 
+  .otherwise("/");
+    $locationProvider.html5Mode(true);
+}]);
