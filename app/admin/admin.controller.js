e83e68039117347e4a06729aefb591b3b184c5ba
daniellindkarlberg <danielskua@hotmail.com>
2017-03-16 12:06:24 +0100

    added services

---
+++ app/admin/admin.controller.js
@@ -0,0 +1,4 @@
+angular.module("admin").
+controller("adminController", ["$scope", function($scope){
+
+} ]);
---
+++ app/login/login.controller.js
@@ -0,0 +1,4 @@
+angular.module("login").
+controller("loginController", ["$scope", function($scope){
+
+} ]);
