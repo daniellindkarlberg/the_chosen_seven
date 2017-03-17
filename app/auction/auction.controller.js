a3cba7a2c3f283ce8924153648bc4d9d25ecc816
Christian Löwendahl <christian.lowendahl@gmail.com>
2017-03-17 13:32:03 +0100

    Filter på kategori

--- app/auction/auction.controller.js
+++ app/auction/auction.controller.js
@@ -7,23 +7,24 @@ controller("auctionController", ["$scope", "$location", "auctionService", functi
 
             });
             $scope.auctions = auctions;
+            console.log(auctions[0]);
     });
 
-
- //auctionService.getCategories().then(function(response) {
-   //     var categories = response.data;
+ auctionService.getCategories().then(function(response) {
+        var categories = response.data;
         
-     //   categories.unshift({ name:"Alla kategorier" });
-       //     console.log(categories);
-         //   $scope.categories = categories;
-  //  });
+        categories.unshift({ name:"Alla kategorier" });
+            $scope.categories = categories;
+    });
 
-
+$scope.onCategorySelect = function(categoryId) {
+    console.log("Selected category Id:" + categoryId);
+    $scope.selectedCategoryId = categoryId;
+};
 
 $scope.auctionClicked = function (id) {
         $location.path("/auction/" + id);
-
-        console.log(id);
+        // console.log(id);
     }
 
 
