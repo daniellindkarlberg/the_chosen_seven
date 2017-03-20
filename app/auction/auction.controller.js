d74bff106d5dbe7e6f17d9df591bb7ac34c3a79e
daniellindkarlberg <danielskua@hotmail.com>
2017-03-20 15:54:16 +0100

    highest bid auctionlist

--- app/auction/auction.controller.js
+++ app/auction/auction.controller.js
@@ -1,33 +1,49 @@
 angular.module("auction").
-controller("auctionController", ["$scope", "$location", "auctionService", function($scope, $location, auctionService){
+    controller("auctionController", ["$scope", "$location", "auctionService", function ($scope, $location, auctionService) {
 
-    auctionService.getAuctions().then(function(response) {
-        var auctions = response.data;
-            angular.forEach(auctions, function(auction) {
 
-                var highestBid = auction[auction.length - 1];
+
+
+
+        auctionService.getAuctions().then(function (response) {
+            var auctions = response.data;
+
+            angular.forEach(auctions, function (auction) {
+
+                auctionService.getAuctionBids(auction.id).then(function (response) {
+                    var bids = response.data;
+
+                    if (bids[0].auctionId == auction.id) {
+
+                        auction.highestBid = bids[bids.length - 1].bidPrice;
+
+                    }
+
+                });
 
             });
+
+
             $scope.auctions = auctions;
-            // console.log(auctions[0]);
-    });
 
- auctionService.getCategories().then(function(response) {
-        var categories = response.data;
-        categories.unshift({ name:"Alla kategorier" });
+        });
+
+        auctionService.getCategories().then(function (response) {
+            var categories = response.data;
+            categories.unshift({ name: "Alla kategorier" });
             $scope.categories = categories;
-    });
+        });
 
-$scope.onCategorySelect = function(categoryId) {
-    // console.log("Selected category Id:" + categoryId);
-    $scope.selectedCategoryId = categoryId;
-};
+        $scope.onCategorySelect = function (categoryId) {
+            // console.log("Selected category Id:" + categoryId);
+            $scope.selectedCategoryId = categoryId;
+        };
 
-$scope.auctionClicked = function (id) {
-        $location.path("/auction/" + id);
-    }
+        $scope.auctionClicked = function (id) {
+            $location.path("/auction/" + id);
+        }
 
 
-}]);
+    }]);
 
 
