e81f0e3c00ab8f923571560efbb80882bcda7523
TheLaMarty <martinaalevin@gmail.com>
2017-03-20 09:38:59 +0100

    admin-controller

--- app/admin/admin.controller.js
+++ app/admin/admin.controller.js
@@ -1,8 +1,35 @@
-angular.module("admin").
-controller("adminController", ["$scope", function($scope, adminService){
+angular.module("admin").controller("adminController", ["$scope", "auctionService", "supplierService", function ($scope, auctionService, supplierService) {
 
-    adminService.getClosedAuctions().then(function(response){
-                    $scope.closedAuctions = response.data;
-                    console.log(response.data);
+    var completedAuctions;
+    var suppliers;
+    
+    $scope.comepletedAuctions = auctionService.getCompletedAuctions();
+
+    auctionService.getCompletedAuctions().then(function (response) {
+        $scope.completedAuctions = response.data;
+        completedAuctions = response.data;
+        console.log(response.data);
+    });
+
+    $scope.suppliers = supplierService.getSuppliers();
+
+    supplierService.getSuppliers().then(function (response) {
+        $scope.suppliers = response.data;
+        suppliers = response.data;
+        console.log(response.data);
+    });
+
+angular.forEach(completedAuctions, function(auction) {
+    angular.forEach(suppliers, function(supplier){
+        if(auction.supplierId == supplier.id){
+         auction.supplierName = supplier.name;
+         auction.supplierCommission = supplier.commission * auction. 
+        }
+    });
+
+    
 });
-} ]);
+
+}]);
+
+
