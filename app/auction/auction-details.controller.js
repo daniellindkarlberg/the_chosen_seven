7f66914bff1c92887c8a999653d8a72ee9af3fec
daniellindkarlberg <danielskua@hotmail.com>
2017-03-20 14:36:48 +0100

    auction details etc

--- app/auction/auction-details.controller.js
+++ app/auction/auction-details.controller.js
@@ -1,52 +1,110 @@
 angular.module("auction").
-controller("auctionDetailsController", ["$scope", "$routeParams", "auctionService","supplierService" , "loginService",
+    controller("auctionDetailsController", ["$scope", "$routeParams", "auctionService", "supplierService", "loginService", "$timeout", "$location",
 
-function($scope, $routeParams, auctionService, supplierService, loginService) {
-   var auction = {};
-   
-       auctionService.getAuctionById($routeParams.auctionId).then(function(response) {
-        auction = response.data; 
-        $scope.auction = auction;
-
-        supplierService.getSupplierById(auction.supplierId).then(function(response) {
-        $scope.supplier = response.data;
-
-       
-        auctionService.getAuctionBids(auction.id).then(function(response) {
-        $scope.bids = response.data;
-
-        console.log(response.data);
-
-       
-
-     
-    }, function(errorResponse) {
-        
-    });
-
-     
-    }, function(errorResponse) {
-        
-    });
-      
-       
-
-     
-    }, function(errorResponse) {
-        
-    });
-
-
- 
+        function ($scope, $routeParams, auctionService, supplierService, loginService, $timeout, $location) {
+            var auction = {};
+            var customerId = loginService.getCustomerId();
+            $scope.bid = {};
 
 
 
+            auctionService.getAuctionById($routeParams.auctionId).then(function (response) {
+                auction = response.data;
+                $scope.auction = auction;
+
+                supplierService.getSupplierById(auction.supplierId).then(function (response) {
+
+                    $scope.supplier = response.data;
 
 
- $scope.isCollapsed = true;
-  $scope.isCollapsedBid = true;
+                    auctionService.getAuctionBids(auction.id).then(function (response) {
+
+                        bids = response.data
+                        var highestBid = bids[bids.length - 1];
+                        $scope.highestBid = highestBid.bidPrice;
+                        $scope.bids = response.data;
+
+                    }, function (errorResponse) {
+
+                    });
+
+
+                }, function (errorResponse) {
+
+                });
+            }, function (errorResponse) {
+
+            });
+
+
+            $scope.updateBidHistory = function () {
+                $timeout(function () {
+                    auctionService.getAuctionBids(auction.id).then(function (response) {
+
+                        $scope.bids = response.data;
+
+                        console.log(response.data);
+                    }, function (errorResponse) {
+
+                    });
+
+                }, 300);
+
+            }
 
 
 
-    
-}]);
+            $scope.buyNowClicked = function (response) {
+                if (!loginService.isLoggedIn()) {
+
+                    $location.path('/login');
+                }
+                else {
+                    var bid = auction.buyNowPrice;
+                    console.log(bid);
+                    var bidObject = {
+                        auctionId: auction.id,
+                        customerId: customerId,
+                        bidPrice: bid
+                    };
+
+                    auctionService.placeBid(bidObject).then(function (response) {
+
+                        $scope.text = "Grattis till inlöpet";
+
+
+                    }, function (errorResponse) {
+
+                    });
+
+                }
+            }
+
+
+            $scope.bidClicked = function (response) {
+                if (!loginService.isLoggedIn()) {
+
+                    $location.path('/login');
+                }
+                else {
+                    var bid = $scope.bid.placeBid;
+                    var bidObject = {
+                        auctionId: auction.id,
+                        customerId: customerId,
+                        bidPrice: bid
+                    };
+
+                    auctionService.placeBid(bidObject).then(function (response) {
+
+                        $scope.text = "Bud lagt!"
+                    }, function (errorResponse) {
+
+                    });
+
+                }
+
+            }
+            $scope.isCollapsed = true;
+            $scope.isCollapsedBid = true;
+
+        }]);
--- app/auction/auction.service.js
+++ app/auction/auction.service.js
@@ -1,27 +1,32 @@
 angular.module("auction")
-.factory("auctionService", ["$http", function($http) {
-    return {
-        getAuctions: function() { 
-            return $http.get("http://nackademiska-api.azurewebsites.net/api/auction"); 
-        },
-        getAuctionById: function(id) {
-            return $http.get("http://nackademiska-api.azurewebsites.net/api/auction/" + id);
-        },
-
-        getCompletedAuctions: function() { 
-            return $http.get("http://nackademiska-api.azurewebsites.net/api/auction/completed"); 
-        },
-
-        getAuctionBids: function(id) {
-            return $http.get("http://nackademiska-api.azurewebsites.net/api/bid/" + id);
-        },
-
-         getCategories: function() {
-            return $http.get("http://nackademiska-api.azurewebsites.net/api/category");
-        },
-         placeBid: function(auctionId,customerId, bidPrice) {
-            return $http.post("http://nackademiska-api.azurewebsites.net/api/bid"), {auctionId: auctionId, customerId:customerId, bidPrice:bidPrice};
-        },
+    .factory("auctionService", ["$http", function ($http) {
         
-    };
-}]);
+
+        return {
+            getAuctions: function () {
+                return $http.get("http://nackademiska-api.azurewebsites.net/api/auction");
+            },
+            getAuctionById: function (id) {
+                return $http.get("http://nackademiska-api.azurewebsites.net/api/auction/" + id);
+            },
+
+            getCompletedAuctions: function () {
+                return $http.get("http://nackademiska-api.azurewebsites.net/api/auction/completed");
+            },
+
+            getAuctionBids: function (id) {
+                return $http.get("http://nackademiska-api.azurewebsites.net/api/bid/" + id);
+            },
+
+            getCategories: function () {
+                return $http.get("http://nackademiska-api.azurewebsites.net/api/category");
+            },
+            placeBid: function (bid) {
+                return $http.post("http://nackademiska-api.azurewebsites.net/api/bid", bid);
+            },
+
+
+
+
+        };
+    }]);
