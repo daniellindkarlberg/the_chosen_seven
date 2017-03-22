bf41ce5b83c1badfa43deaec35522d2120d99439
Christian Löwendahl <christian.lowendahl@gmail.com>
2017-03-22 11:24:28 +0100

    Fixat säljrapport! :-)

--- app/admin/admin.controller.js
+++ app/admin/admin.controller.js
@@ -5,59 +5,48 @@ angular.module("admin").controller("adminController", ["$scope", "auctionService
 
         auctionService.getCompletedAuctions().then(function (response) {
             completedAuctions = response.data;
-            // console.log("First completed auction id: " + completedAuctions[0].id);
 
             supplierService.getSuppliers().then(function (response) {
                 suppliers = response.data;
-                // console.log("First supplier name: " + suppliers[0].companyName);
 
                 angular.forEach(completedAuctions, function (auction, index) {
                     angular.forEach(suppliers, function (supplier) {
                         if (auction.supplierId == supplier.id) {
                             auction.supplierName = supplier.companyName;
-                            // console.log("Supplier name: " + auction.supplierName);
                             auction.commissionRate = supplier.commission;
-                            // console.log("Supplier commission rate: " + auction.commissionRate);
                         }
                     });
 
-                    // console.log("Auction id for bids: " + auction.id);
                     auctionService.getAuctionBids(auction.id).then(function (response) {
                         var bids = response.data;
                         var highestBid = bids[bids.length - 1];
-                        // console.log(highestBid);
                         auction.highestBid = highestBid.bidPrice;
                         auction.dateOfHighestBid = highestBid.dateTime;
-                        // console.log("Highest bid: " + auction.highestBid);
                         auction.commission = auction.commissionRate * auction.highestBid;
-                        // console.log("Commission: " + auction.commission);
-                        console.log(index);
-                        if(index == completedAuctions.length - 1) {
-                             $scope.makeSalesMonths();
-                        }
-                        
+                        //if(index == completedAuctions.length - 1) {
+                        //    $scope.makeSalesMonths();
+                        //} 
                     });
                
                 });
 
-                
-
-                // console.log("Commission for completed auction: " + completedAuctions[0].commission);
-
                 $scope.completedAuctions = completedAuctions;
 
             });
+
         });
 
         // Function to run inside the AJAX sequence.
           $scope.makeSalesMonths = function () {
-                    console.log("Starting function makeSalesMonths");
+                    // console.log("Starting function makeSalesMonths");
 
                     angular.forEach(completedAuctions, function (auction) {
-                        console.log(auction);
+                        // console.log(auction);
                         if (auction.highestBid == auction.buyNowPrice) {
+                            // console.log("Buy now price was paid");
                             auction.finishedTime = auction.dateOfHighestBid;
                         } else {
+                            // console.log("End time was reached");
                             auction.finishedTime = auction.endTime;
                         }
                         // console.log(auction.finishedTime);
@@ -65,26 +54,29 @@ angular.module("admin").controller("adminController", ["$scope", "auctionService
                         var finishedMonth = finishedDate.getMonth();
                         // console.log("Finished date: " + finishedDate);
                         // console.log("Finished month: " + finishedMonth);
-                        auction.endMonth = new Date(auction.finishedTime);
+                        auction.finishedMonth = finishedMonth;
                         //console.log("auction.endMonth: " + auction.endMonth);
                     });
 
                     var salesMonths = [{ name: "Januari" }, { name: "Februari" }, { name: "Mars" }, { name: "April" }, { name: "Maj" }, { name: "Juni" },
                     { name: "Juli" }, { name: "Augusti" }, { name: "September" }, { name: "Oktober" }, { name: "November" }, { name: "December" }];
 
-                    console.log("Name of first month: " + salesMonths[0].name);
-
                     angular.forEach(salesMonths, function (salesMonth, index) {
-                        salesMonth.sales = 0;
-                        angular.forEach(completedAuctions, function (auction) {
+                        salesMonth.totalSales = 0;
+                        salesMonth.totalCommission = 0;
+                        angular.forEach(completedAuctions, function (auction,index2) {    
                             // console.log("auction.endMonth: " + auction.endMonth);
                             // console.log("index: " + index);
-                            // console.log("auction.highestBid: " + auction.highestBid);
-                            if (auction.endMonth == index) {
-                                salesMonth.sales += auction.highestBid;
+                            // console.log("auction.finishedMonth: " + auction.finishedMonth);
+                            // console.log("auction.commission: " + auction.commission);
+                            if (auction.finishedMonth == index) {
+                                // console.log("commission to add: " + auction.commission)
+                                salesMonth.totalSales += auction.highestBid;
+                                salesMonth.totalCommission += auction.commission;
                             };
                         });
-                        console.log("Sales during month: " + salesMonth.name + " was: " + salesMonth.sales);
+                        // console.log("Total sales during month: " + salesMonth.name + " was: " + salesMonth.totalSales);
+                        // console.log("Commission during month: " + salesMonth.name + " was: " + salesMonth.totalCommission);
                     });
                     $scope.salesMonths = salesMonths;
                 }
